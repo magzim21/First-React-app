5287c2da72e1eeb95df2850e1c0c5eaf95dd3a0b
Maksym Onyshchenko <magzim@Maksyms-MacBook-Pro.local>
2019-09-15 17:31:15 +0300

    sdf

---
+++ precache-manifest.49efaa043bc56fe2fb84afe770ca3690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f08e142f2d5bcd34f547ac73b5ae9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997f968703194ada743b",
+    "url": "/static/css/main.e6378e7c.chunk.css"
+  },
+  {
+    "revision": "58eaf4bbd044d81d36a4",
+    "url": "/static/js/2.3c1c5b70.chunk.js"
+  },
+  {
+    "revision": "997f968703194ada743b",
+    "url": "/static/js/main.34ab8230.chunk.js"
+  },
+  {
+    "revision": "06f36934a1ece9234f65",
+    "url": "/static/js/runtime~main.8d3dc686.js"
+  }
+]);
